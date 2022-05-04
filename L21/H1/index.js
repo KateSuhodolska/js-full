5017ecb8a6e2743286302d2fa8555f40c88dfc62
Ekaterina Suhodolskaya <90890687+KateSuhodolska@users.noreply.github.com>
2022-05-04 10:12:51 +0300

    lessons

--- L21/H1/index.js
+++ L21/H1/index.js
@@ -1,10 +1,12 @@
 export const getTitleElement = () => {
     const titleElem = document.querySelector(".title");
     console.dir(titleElem);
+    return titleElem;
 };
 
 export const getInputElement = () => {
     const inputElem = document.querySelector("input");
     inputElem.getAttribute("type", "text");
     console.dir(inputElem);
+    return inputElem;
 };
