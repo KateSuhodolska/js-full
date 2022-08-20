bbcbc8a69aaceb500d40a93a0c4c05f57f0d6c3a
Ekaterina Suhodolskaya <90890687+KateSuhodolska@users.noreply.github.com>
2022-08-20 20:46:22 +0300

    lessonso

--- L34/H2/index.js
+++ L34/H2/index.js
@@ -2,32 +2,34 @@ const baseUrl =
     "https://62f900f53eab3503d1decd9d.mockapi.io/api/v1/users/user-form";
 
 const registerButton = document.querySelector(".submit-button");
-const emailInput = document.querySelector('.form-input[type="email"]');
-const textInput = document.querySelector('.form-input[type="text"]');
-const passwordInput = document.querySelector('.form-input[type="password"]');
+const email = document.querySelector('.form-input[type="email"]');
+const text = document.querySelector('.form-input[type="text"]');
+const password = document.querySelector('.form-input[type="password"]');
 const loginForm = document.querySelector(".login-form");
 
 const isValid = () => {
     if (
-        emailInput.reportValidity() &&
-        textInput.reportValidity() &&
-        passwordInput.reportValidity()
+        email.reportValidity() &&
+        text.reportValidity() &&
+        password.reportValidity()
     ) {
-        registerButton.disabled = false;
+        registerButton.removeAttribute("disabled");
     }
 };
-
-// const isValid = () => {
-//     if (loginForm.reportValidity()) {
-//         registerButton.removeAttribute("disabled");
-//     } else registerButton.setAttribute("disabled", true);
-// };
 
 const getData = () =>
     fetch(baseUrl)
     .then((response) => response.json())
     .then((users) => users[users.length - 1])
-    .then((data) => alert(JSON.stringify(data)));
+    .then((data) =>
+        alert(
+            JSON.stringify({
+                email: data.email,
+                name: data.name,
+                password: data.password,
+            })
+        )
+    );
 
 const sendData = (event) => {
     event.preventDefault();
