7dce03191757ea3e775c49068dffeee643d8e5b7
Ekaterina Suhodolskaya <90890687+KateSuhodolska@users.noreply.github.com>
2022-08-23 14:39:06 +0300

    lessons

--- L35/C3/gateways.js
+++ L35/C3/gateways.js
@@ -1,9 +1,9 @@
 export const fetchUserData = (userName) =>
     fetch(`https://api.github.com/users/${userName}`)
     .then((response) => response.json())
-    .catch((err) => alert(err));
+    .catch(() => alert("Failed to load data"));
 
 export const fetchRepo = (url) =>
     fetch(url)
     .then((response) => response.json())
-    .catch((err) => alert(err));
+    .catch(() => alert("Failed to load data"));
